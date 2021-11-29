830f4da03c431f6c617da1987b7d64e87909257b
Willian Komada <willian10komada@hotmail.com>
2021-11-29 16:04:07 -0300

    Rotas e permissões de usuário

---
+++ src/database/migrations/1638212622372-CreateUsersRoles.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsersRoles1638212622372 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/repositories/RoleRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from "typeorm";
+import Role from "../models/Role";
+
+
+@EntityRepository(Role)
+class RoleRepository extends Repository<Role> {
+
+}
+
+export default RoleRepository;
