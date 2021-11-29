8db55e45e819d87f50c1d7495e657057c431b1ea
Willian Komada <willian10komada@hotmail.com>
2021-11-29 12:17:58 -0300

    initial config

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # backend-permissions-user
+
 Esse projeto foi desenvolvido com o objetivo de aprender a como criar controle de acesso de usuário, utilizando NodeJS.
+
+Passo à passo da construção da aplicação Back-end
+
+[] Cadastro de Users
+[] Autenticação de Users
+[] Cadastro de Permissions
+[] Cadastro de Roles
+[] Cadastro de Products
+[] Conexão de User_Roles
+[] Conexão de Permission_Roles
---
+++ src/database/migrations/1638197705654-CreateUsers.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1638194369400 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'gen_random_uuid()'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'username',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
